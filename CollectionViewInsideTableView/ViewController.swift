509c63eace7065f59206558826dbb8a90fc611aa
sungyoungPark <sygitter1491012@gmail.com>
2022-06-14 20:34:28 +0900

    Initial Commit

---
+++ CollectionViewInsideTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewInsideTableView
+//
+//  Created by 박성영 on 2022/06/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
